16025d295b7622c06fd58b08c1b855b16c55c4d9
shaki.DE <sifatshaki7@gmail.com>
2023-10-01 00:41:15 -0400

    add new pacakge and new line

---
+++ src/test/java/gitHubPractice/TestPractice.java
@@ -0,0 +1,23 @@
+package gitHubPractice;
+
+public class TestPractice {
+    public static void main(String[] args) {
+        System.out.println("practice new line to gitHub");
+        System.out.println("delaware is the first state of USA");
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
